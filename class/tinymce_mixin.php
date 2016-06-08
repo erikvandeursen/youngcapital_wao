887140613d701fe49ca3ad9c464d26d57e709e78
erikvandeursen <erikvandeursen@gmail.com>
2016-06-09 00:46:56 +0200

    Add files via upload

---
+++ class/tinymce_mixin.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<form method="post" action=$query>
+	<h2>Titel</h2>
+	<input type="text" name="admblogtitle"></input><br>
+	<h2>Intro</h2>
+	<textarea class="tinymcetextarea" name="admblogintro"></textarea>
+	<h2>Content</h2>
+	<textarea class="tinymcetextarea" name="admblogcontent"></textarea>
+	<input type="submit" name = "admblogsubmit" value="Opslaan op pagina"></input>
+</form>;
